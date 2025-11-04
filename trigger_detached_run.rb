4f61b7efb177690f14cd9334fcd5f58f65487780
Adrien Di Pasquale <adrien@dipasquale.fr>
2025-11-04 15:38:12 +0100

    nightly cron jobs to trigger detached runs for all 3 apps

---
+++ trigger_detached_run.rb
@@ -0,0 +1,35 @@
+require 'optparse'
+
+APPS = ["rdvi", "rdvsp", "rdvs"].freeze
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} --app APP"
+  opts.on("-a", "--app APP", "Specify the app (rdvi, rdvsp, rdvs)") do |a|
+    options[:app] = a
+  end
+end.parse!
+
+app = options[:app]
+raise ArgumentError, "App option is required" if app.nil?
+
+if APPS.exclude?(app)
+  raise ArgumentError, "App must be one of: #{APPS.join(', ')}"
+end
+
+if system("which scalingo")
+  puts "⏭️ scalingo-cli already installed…"
+else
+  res = system("install-scalingo-cli")
+  raise "❌ Failed to install scalingo CLI" unless res
+end
+
+# scalingo is auto logged in via environment variable SCALINGO_API_TOKEN
+
+command = %[scalingo --region osc-secnum-fr1 --app rdv-service-public-etl run --detached "bundle exec ruby main.rb --app #{app}"]
+
+puts "Executing command: #{command}…"
+res = system(command)
+raise "❌ Failed to trigger detached run" unless res
+
+puts "✅ Detached run triggered for app: #{app}"
